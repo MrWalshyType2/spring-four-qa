7639fd6f721944062d4fad10b4926f71bcfe01a1
MrWalshyType2 <morgan.walsh@academytrainee.com>
2020-07-28 13:59:28 +0100

    added record controller

---
+++ src/main/java/com/example/controller/RecordController.java
@@ -0,0 +1,27 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/record")
+public class RecordController {
+
+	@GetMapping()
+	public String message1() {
+		return "You sent a GET request, reponse success";
+	}
+	
+	@PostMapping()
+	public String message2() {
+		return "You sent a POST request, response success";
+	}
+	
+	@DeleteMapping()
+	public String message3() {
+		return "You sent a DELETE request, response success";
+	}
+}
